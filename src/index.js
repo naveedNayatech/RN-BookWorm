2b024882027479192303534b1acbf1e1cd390046
naveed_hevitt.com <naveed.pieces02@gmail.com>
2025-07-05 21:07:58 +0500

    cron job added

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import job from "./lib/cron.js";
 dotenv.config();
 
 import authRoutes from "./routes/authRoutes.js";
@@ -38,6 +39,7 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
