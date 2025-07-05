09e880523f101f1ff25131342c3c9f46775a96d4
naveed_hevitt.com <naveed.pieces02@gmail.com>
2025-07-05 19:59:26 +0500

    PORT changed to 8080

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
